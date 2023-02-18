5f9140e64648c9ea94059b938f7cf1989bba89a1
01manojkumar <124807798+01manojkumar@users.noreply.github.com>
2023-02-18 16:12:10 +0530

    Update App.js
    added comments

--- src/App.js
+++ src/App.js
@@ -32,17 +32,44 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function App() {
+  
+  //these two are the methods of material UI --- 1 it is in built function
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
+  //------------3-----------------state for handling posts
   const [posts, setPosts] = useState([]);
+  
+  //------------4--------------------------- state for handling USENAME in the input form 
   const [username, setUsername] = useState("");
+  
+  //--------------------------------5---------------- state for handling E-MAIL in the input form 
   const [email, setEmail] = useState("");
+  
+  // --------------------------6-----------------------------state for handling PASSWORD in the input form 
   const [password, setPassword] = useState("");
+  
+  //-------------------------7-------------------this state is used for authentification and used in login and signup in UseEffect functions
   const [user, setUser] = useState(null);
+  
+  
+  //this is used for openning and closing of modal in which we are making a login form
   const [open, setOpen] = useState(false);
   const [registerOpen, setRegisterOpen] = useState(false);
+  
 
+  
+  //------------------------------------DEFINITION AND WORKING OF USE-EFFECT FUNCTION-------------------------------------------------------
+  //----------------------------------------------------------------------------------------------------------------------------------------
+  /* useEffect == runs a  code based on a specific conditions
+  useEffect{() => {this is where we write our code}
+  ,[here we write our conditions]
+  } it will run when app component load and second time when changes are made in the conditions
+  ---------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
+  */
   useEffect(() => {
+    //here we are using unsubscribe to perform some clean up if this code runs multiple times
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         // user is logged in...
@@ -60,21 +87,46 @@ function App() {
         setUser(null);
       }
     });
-
+//this retun we can use in use-effect function 
+    // by calling this it is removing the previous ones 
     return () => {
       unsubscribe();
     };
   }, [user, username]);
+  
+  
+  
+  /* use effect functions of posts
+  
+  snapshot ==== every single time when there is some changes in the db it will catch it
+  
+  
+  */
 
   useEffect(() => {
     db.collection("posts")
       .orderBy("timestamp", "desc")
       .onSnapshot((snapshot) =>
         setPosts(snapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() })))
+                  //without key it will refresh all the posts /
+                  //here we a returning a objectwhich contains id and posts
+                  //every time a post is added this code runs
       );
   }, []);
+  
+  
+  
+  
+  /* these are the two functions which are assosiated with the two buttuons
+  1---- handle login
+  2======handle register
+  
+  */
 
+  //---------------------------------------------------------------------------------------------------------------------------------
+  
   const handleLogin = (e) => {
+    //when we use a form its default behaviour when submit is done is to reload to prevent that from happing we use event.prevent function
     e.preventDefault();
     auth
       .signInWithEmailAndPassword(email, password)
@@ -83,18 +135,48 @@ function App() {
     setOpen(false);
   };
 
+  
+  
+  
+  
+  
   const handleRegister = (e) => {
+    //when we use a form its default behaviour when submit is done is to reload to prevent that from happing we use event.prevent function
     e.preventDefault();
     auth
+    //this is defaul function from the firebase
       .createUserWithEmailAndPassword(email, password)
+    // these email and password are from the states//
       .catch((error) => alert(error.message));
 
     setRegisterOpen(false);
   };
 
+ 
+  
+  /*above this is the handle event functions
+  
+  --------------------------------------------------------------------------------------------
+  ----------------------------------------------
+  here return of app functional component starts---*/
+  
+  
   return (
     <div className="app">
-      <Modal open={open} onClose={() => setOpen(false)}>
+    
+    //here we are using two modals one for login and other for signup
+    
+    
+    
+    /*this modal is for login */
+    //here we are using material UI modal
+    
+    
+    
+    // MODAL NUMBER -----------------ONE
+    
+      <Modal open={open} onClose={() => setOpen(false)}> // either we can make a handle close function which will SetOpen falce 
+    //or we can make a inline function which will perform the same work on less code
         <div style={modalStyle} className={classes.paper}>
           <form className="app__login">
             <center>
@@ -117,10 +199,22 @@ function App() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <Button onClick={handleLogin}>Login</Button>
+            <Button onClick={handleLogin}>Login</Button>  
           </form>
         </div>
       </Modal>
+
+
+/* this modal is for signup which contains 4 things
+1-ip for email
+2--ip for username
+3---ip for password
+4-- button to trigger a function
+
+*/
+
+
+//MODAL NUMBER =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-TWO
 
       <Modal open={registerOpen} onClose={() => setRegisterOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
@@ -154,6 +248,11 @@ function App() {
           </form>
         </div>
       </Modal>
+//Header is here 
+
+//you should make it a different component remove it from appppp---------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -177,10 +276,25 @@ function App() {
         )}
       </div>
 
+// app header termminates here
+//---------------------------------------------------------------------------------------------------------------------------
+//=============================================================================================================================4
+//==============================================================================================================================
+
+
+
       <div className="app__posts">
         <div className="app__postsLeft">
           <FlipMove>
+          
+          /* here we are using a map function you can relate to a loop and it is fetching data ,here from database*/
+          
             {posts.map(({ id, post }) => (
+              
+              /* imported the post component and fiving it props
+              after this we are generating the dynamic post component not the static ones which we used in initial stages
+              */
+              
               <Post
                 user={user}
                 key={id}
@@ -192,6 +306,8 @@ function App() {
             ))}
           </FlipMove>
         </div>
+
+
         <div className="app__postsRight">
           <InstagramEmbed
             url="https://www.instagram.com/p/B_uf9dmAGPw/"
@@ -210,6 +326,9 @@ function App() {
 
       {user?.displayName ? (
         <div className="app__upload">
+        
+  // -------------------------------------------------------------IMAGE --UPLOAD --COMPONENT COMES HERE ------------------------------//
+        // WE ARE JUST PASSING ONE PROPS=========
           <ImageUpload username={user.displayName} />
         </div>
       ) : (
